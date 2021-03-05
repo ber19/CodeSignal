7c7622b03e061fd4d7dd5b762050c98b0b0ed2ae
BerFlores <berflores19@gmail.com>
2021-03-05 17:44:07 -0600

    objetos_iterables

--- objetos_iterables.py
+++ objetos_iterables.py
@@ -4,7 +4,8 @@ string = "1234576"
 sett = {1,2,3,4,5,7,6}
 dicc = {'uno':1, 'dos':2, 'tres':3, 'cuatro':4, 'cinco':5, 'siete':7, 'seis':6}
 
-
-prueba = 
-print(prueba)
-print(type(prueba))
+lista_dicc = [ [1,2], (3,[3.1,3.2]), '52', {(7.1,7.2),7} ]
+dicc_lista = dict(lista_dicc)
+for i in dicc_lista.items():
+    k, v = i
+    print(f'{k} -> {v}')
